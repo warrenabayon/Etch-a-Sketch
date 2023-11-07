8e5bd7c26818c94fab97f2aaabc37098e96e62a0
Waron <warrenabayon@gmail.com>
2023-11-08 00:32:09 +0800

    code redo for practice

--- js/app.js
+++ js/app.js
@@ -1,60 +1,82 @@
 
-const createSketchPad = () => {
-  //create main div
+function createSketchPad() {
+
   const body = document.querySelector('body');
-  const container = document.createElement('div');
-  container.setAttribute('class', 'container');
-  body.appendChild(container);
+  const createBtn = document.createElement('button');
+  const resetBtn = document.createElement('button');
+  resetBtn.classList.add('reset');
+  resetBtn.textContent = "RESET";
+  createBtn.classList.add('crtBtn');
+  createBtn.textContent = 'Create Sketch Pad';
+  body.appendChild(createBtn);
 
+  const makeSquares = ()=> {
 
-
+    const checkIfExist = ()=> {
+      const cnt = document.querySelector('.container');
+      if (cnt !==null) {
+        cnt.remove();
+        createInsideDiv();
+      } else {
+        createInsideDiv();
+      }
+    }      
   
-      const resetBtn = document.createElement('button');
-      resetBtn.setAttribute('class', 'reset');
-      resetBtn.textContent = 'RESET';
+    checkIfExist();
+
+    function createInsideDiv() {
+      const container = document.createElement('div');
+      container.classList.add('container');
+      body.appendChild(container);
       body.appendChild(resetBtn);
+      const squareNumber = prompt('Enter number of squares per side');
+
+      const changeColor = ()=> {
+        const rainbowColors = ['red', 'orange', 'yellow','green', 'blue', 'indigo', 'violet']
+        const ranNumber = Math.floor(Math.random() * rainbowColors.length);
+        // console.log(ranNumber);
+        event.target.style.backgroundColor = `${rainbowColors[ranNumber]}`;
+      }
+
+
+
+      if (+squareNumber < 100){
+        // container.style.gridTemplateColumns = `repeat(${squareNumber}, 1fr)`;
+        container.setAttribute('style', `grid-template-columns: repeat(${+squareNumber}, 1fr);`);
+
+        
+
+        for (i = 0; i < (+squareNumber * +squareNumber); i++){
+
+          
+
+          const square = document.createElement('div');
+          square.setAttribute('class', `square sr${i}`);
+          container.appendChild(square);
+          
+          
+          square.addEventListener('mouseover',changeColor);
+
+          resetBtn.addEventListener('click', ()=> {
+            square.style.backgroundColor = '#fff';
+          })
+        } 
+
+        
+
+      } 
       
- 
+
+      }
+
   
-
- 
-  const createGrid = () => {
-    const sqaureCount = prompt('How many squares ? by ?','0');
-    container.style.gridTemplateColumns = `repeat(${sqaureCount}, 1fr)`;
-
-
-    const changeColor = () => {
-      event.target.setAttribute('style', 'background-color: #8b8a8a;');
-    };
-
-
-    for (i = 0; i < +sqaureCount * +sqaureCount; i++) {
-      const square = document.createElement('div');
-      square.setAttribute('class', `square sq${i}`);
-      container.appendChild(square);
-
-      square.addEventListener('mouseover', changeColor);     
-     
-      resetBtn.addEventListener('click', ()=>{
-        square.setAttribute('style', 'background-color: #ffffff');
-      });
-    }
-
-    
   };
-
-  createGrid();
-
   
 
+  createBtn.addEventListener('click', makeSquares);
   
 
-
-  
-
-  
-  
-  
 }
+
 
 createSketchPad();
